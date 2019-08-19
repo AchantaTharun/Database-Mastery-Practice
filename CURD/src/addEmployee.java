cda5fb38f19ea935036367479a5e8b8d803655cf
Tharun <achantatharun0@gmail.com>
2019-08-19 18:17:55 +0530

    CURD TASK COMPLETE

--- CURD/src/addEmployee.java
+++ CURD/src/addEmployee.java
@@ -6,6 +6,7 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -45,17 +46,28 @@ public class addEmployee extends HttpServlet {
 		response.setContentType("text/html");  
         PrintWriter out=response.getWriter();  
           
-        String name=request.getParameter("ename");  
-        String password=request.getParameter("pword");  
-        String email=request.getParameter("email");  
+        String ename=request.getParameter("ename");  
+        String pword=request.getParameter("pword");  
+        String email=request.getParameter("mail");  
         String job=request.getParameter("job");  
         
-        String sal=request.getParameter("sal");  
+        String sa=request.getParameter("sal");
         String phno=request.getParameter("phno");  
-        
+        //out.println(email);
       //  out.println(name);
+        int i=0;
         
-        try{   
+        
+        if(ename=="" || pword=="" || email=="" || job=="" ||sa=="" ||phno=="") {
+        	RequestDispatcher rd=request.getRequestDispatcher("index.html");
+        	rd.include(request, response);
+        	
+        	out.println("please fill all details");
+        	
+        }else {
+        	try{   
+        		
+        		int sal=  Integer.parseInt(sa);
     			Class.forName("oracle.jdbc.driver.OracleDriver");  
     		    
     			Connection con=DriverManager.getConnection(  
@@ -63,13 +75,24 @@ public class addEmployee extends HttpServlet {
     			 
     			Statement stmt=con.createStatement();  
     			 
-    			ResultSet rs=stmt.executeQuery("select count(*)  from emp");    
-    			rs.next();
-    			System.out.println(rs.getString(1));  
+    			i=stmt.executeUpdate("insert into emp_test(ename,job,sal,email,phno,password) values('"+ename+"','"+job+"','"+sal+"','"+email+"','"+phno+"','"+pword+"')");    
+    			if(i==1) {
+    				RequestDispatcher rd=request.getRequestDispatcher("index.html");
+    	        	rd.include(request, response);
+    				out.println("Inserted successfully");
+    			}else {
+    				RequestDispatcher rd=request.getRequestDispatcher("index.html");
+    	        	rd.include(request, response);
+    				out.println("Insertion failed");
+    			}
     			con.close();  
     			 
     			}catch(Exception e){ System.out.println(e);}  
+        	out.println("<br><br><a href='home.html'>back</a>");
     			  
+        	
+        }
+        
     			
 
 	}
